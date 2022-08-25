aa64736328e62f9dcc2acc486b3dc32b4f3775c9
eli7pm <48443245+eli7pm@users.noreply.github.com>
2022-08-25 19:04:02 -0400

    Update PdfViewerComponent.js

--- src/components/PdfViewerComponent.js
+++ src/components/PdfViewerComponent.js
@@ -51,7 +51,7 @@ export default function PdfViewerComponent(props) {
 				customFonts: customFontsRef.current,
 				//password: userPassword,
 				// Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
-				baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
+				baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}/`,
 				//password: "secr3t",
 
 				toolbarItems: [
